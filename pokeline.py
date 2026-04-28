b645c0afd42e4de9941bb424bfdccc8290ad6bea
ShadowKuro69 <fackyoulol123@gmail.com>
2026-04-28 17:42:47 +0530

    Added rich and colors to output

--- pokeline.py
+++ pokeline.py
@@ -1,26 +1,28 @@
-import requests 
-
+import requests
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
+console = Console()
 
 
 def get_pokemon(name):
-        url = f"https://pokeapi.co/api/v2/pokemon/{name.lower()}"
-        response = requests.get(url)
-        data = response.json()
-        print(data["name"])
-        print(data["base_experience"])
+    url = f"https://pokeapi.co/api/v2/pokemon/{name.lower()}"
+    response = requests.get(url)
+    if response.status_code == 404:
+        console.print("[bold red]Pokemon not found![/bold red]")
+        return
+    data =  response.json()
 
-        for t in data["types"]:
-            print("Type:", t["type"]["name"])
+    console.print(f"\n[bold yellow]{data['name'].upper()}[/bold yellow]")
+    console.print(f"[cyan]Base XP:[/cyan] {data['base_experience']}")
 
-        for stat in data["stats"]:
-            print(stat["stat"]["name"], ":", stat["base_stat"])
+    for t in data["types"]:
+        console.print(f"[green]Type:[/green] {t['type']['name']}")
+    for stat in data["stats"]:
+            console.print(f"[magenta]{stat['stat']['name']}[/magenta] : {stat['base_stat']}")
 
 while True:
-
-    
-
-
-        name = input("Enter a Pokemon name: ")
-        if name == "quit":
-            break
-        get_pokemon(name)
+    name = input("Enter a Pokemon name: ")
+    if name == "quit":
+        break
+    get_pokemon(name)

dfb75664c8415894c1e5afcedfcb8679d3654505
ShadowKuro69 <fackyoulol123@gmail.com>
2026-04-28 21:00:23 +0530

    Added block art

--- pokeline.py
+++ pokeline.py
@@ -2,6 +2,9 @@ import requests
 from rich.console import Console
 from rich.panel import Panel
 from rich.text import Text
+from PIL import Image
+import io
+
 console = Console()
 
 def get_evolution_chain(name):
@@ -18,6 +21,21 @@ def get_evolution_chain(name):
           chain = chain["evolves_to"][0] if chain["evolves_to"] else None
      return evolutions
         
+
+def show_pokemon_art(sprite_url):
+    response = requests.get(sprite_url)
+    img = Image.open(io.BytesIO(response.content)).convert("RGBA")
+    img = img.resize((32, 32))
+    for y in range(img.height):
+        row = ""
+        for x in range(img.width):
+            r, g, b, a = img.getpixel((x, y))
+            if a < 128:
+                row += "  "
+            else:
+                row += f"[rgb({r},{g},{b})]██[/rgb({r},{g},{b})]"
+        console.print(row)
+
 def get_pokemon(name):
 
     url = f"https://pokeapi.co/api/v2/pokemon/{name.lower()}"
@@ -26,6 +44,9 @@ def get_pokemon(name):
         console.print("[bold red]Pokemon not found![/bold red]")
         return
     data =  response.json()
+    sprite = data["sprites"]["other"]["official-artwork"]["front_default"]
+    if sprite:
+         show_pokemon_art(sprite)
 
     console.print(f"\n[bold yellow]{data['name'].upper()}[/bold yellow]")
     console.print(f"[cyan]Base XP:[/cyan] {data['base_experience']}")
@@ -41,4 +62,4 @@ while True:
     name = input("Enter a Pokemon name: ")
     if name == "quit":
         break
-    get_pokemon(name)
+    get_pokemon(name) 
